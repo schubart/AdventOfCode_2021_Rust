74dd70a3c47999f89871a4417ea8e37f525d6767
Michael Schubart <michael@schubart.net>
2021-12-02 05:34:18 +0000

    Fold

--- day02/src/lib.rs
+++ day02/src/lib.rs
@@ -2,22 +2,23 @@
 #![warn(clippy::pedantic)]
 
 fn part1() -> i32 {
-    let mut h = 0;
-    let mut d = 0;
-    include_str!("input.txt").lines().for_each(|line| {
-        let mut split = line.split(' ');
-        let command = split.next().unwrap();
-        let arg = split.next().unwrap().parse::<i32>().unwrap();
+    let (horizontal, depth) =
+        include_str!("input.txt")
+            .lines()
+            .fold((0, 0), |(horizontal, depth), line| {
+                let mut split = line.split(' ');
+                let command = split.next().unwrap();
+                let arg = split.next().unwrap().parse::<i32>().unwrap();
 
-        match command {
-            "forward" => h += arg,
-            "down" => d += arg,
-            "up" => d -= arg,
-            _ => panic!(),
-        }
-    });
+                match command {
+                    "forward" => (horizontal + arg, depth),
+                    "down" => (horizontal, depth + arg),
+                    "up" => (horizontal, depth - arg),
+                    _ => panic!("Unexpected input: {}", line),
+                }
+            });
 
-    h * d
+    horizontal * depth
 }
 
 #[test]
@@ -26,26 +27,23 @@ fn test_part1() {
 }
 
 fn part2() -> i32 {
-    let mut h = 0;
-    let mut d = 0;
-    let mut aim = 0;
-    include_str!("input.txt").lines().for_each(|line| {
-        let mut split = line.split(' ');
-        let command = split.next().unwrap();
-        let arg = split.next().unwrap().parse::<i32>().unwrap();
+    let (horizontal, depth, _aim) =
+        include_str!("input.txt")
+            .lines()
+            .fold((0, 0, 0), |(horizontal, depth, aim), line| {
+                let mut split = line.split(' ');
+                let command = split.next().unwrap();
+                let arg = split.next().unwrap().parse::<i32>().unwrap();
 
-        match command {
-            "forward" => {
-                h += arg;
-                d += aim * arg;
-            }
-            "down" => aim += arg,
-            "up" => aim -= arg,
-            _ => panic!(),
-        }
-    });
+                match command {
+                    "forward" => (horizontal + arg, depth + aim * arg, aim),
+                    "down" => (horizontal, depth, aim + arg),
+                    "up" => (horizontal, depth, aim - arg),
+                    _ => panic!("Unexpected input: {}", line),
+                }
+            });
 
-    h * d
+    horizontal * depth
 }
 
 #[test]
