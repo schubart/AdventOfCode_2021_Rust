e1f9639ef228813cfebb19a4bfe800fe9b29f40e
Michael Schubart <michael@schubart.net>
2021-12-07 22:59:09 +0000

    Day 5

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -1,38 +1,84 @@
 #![cfg(test)]
 #![warn(clippy::pedantic)]
 
-use std::cmp::max;
-use std::cmp::min;
 use std::collections::HashMap;
+use std::iter::repeat;
 
-fn part1() -> usize {
-    let lines = include_str!("input.txt")
-        .lines()
-        .map(|l| {
-            let split = l.split(&[' ', ','][..]).collect::<Vec<_>>();
-            (
-                split[0].parse::<i32>().unwrap(),
-                split[1].parse::<i32>().unwrap(),
-                split[3].parse::<i32>().unwrap(),
-                split[4].parse::<i32>().unwrap(),
-            )
-        })
-        .filter(|(x1, y1, x2, y2)| x1 == x2 || y1 == y2)
-        .collect::<Vec<_>>();
+type Number = i32;
+type Line = (Number, Number, Number, Number);
 
-    let mut counts = HashMap::new();
-    for (x1, y1, x2, y2) in lines {
-        for x in min(x1, x2)..=max(x1, x2) {
-            for y in min(y1, y2)..=max(y1, y2) {
-                *counts.entry((x, y)).or_insert(0) += 1;
-            }
-        }
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+
+fn parse(text: &str) -> Result<Line> {
+    let split = text.split(&[' ', ','][..]).collect::<Vec<_>>();
+
+    Ok((
+        split[0].parse()?,
+        split[1].parse()?,
+        split[3].parse()?,
+        split[4].parse()?,
+    ))
+}
+
+// Iterates from `a` (inclusive) to `b` (inclusive). Counts down if `a` > `b`.
+fn iterate(a: Number, b: Number) -> Box<dyn Iterator<Item = Number>> {
+    if a < b {
+        Box::new(a..=b)
+    } else {
+        Box::new((b..=a).rev())
     }
+}
+
+fn boxed_repeat(n: Number) -> Box<dyn Iterator<Item = Number>> {
+    Box::new(repeat(n))
+}
+
+fn count_overlaps(lines: &[Line]) -> usize {
+    let mut counts = HashMap::new();
+
+    lines
+        .iter()
+        .flat_map(|&(x1, y1, x2, y2)| {
+            if x1 == x2 {
+                boxed_repeat(x1).zip(iterate(y1, y2)) // Vertical
+            } else if y1 == y2 {
+                iterate(x1, x2).zip(boxed_repeat(y1)) // Horizontal
+            } else {
+                iterate(x1, x2).zip(iterate(y1, y2)) // Diagonal
+            }
+        })
+        .for_each(|p| *counts.entry(p).or_insert(0) += 1);
 
     counts.values().filter(|c| **c > 1).count()
 }
 
+fn part1() -> Result<usize> {
+    let mut lines: Vec<Line> = include_str!("input.txt")
+        .lines()
+        .map(parse)
+        .collect::<Result<_>>()?;
+
+    // Keep only vertical and horizontal lines.
+    lines.retain(|(x1, y1, x2, y2)| x1 == x2 || y1 == y2);
+
+    Ok(count_overlaps(&lines))
+}
+
+fn part2() -> Result<usize> {
+    let lines: Vec<Line> = include_str!("input.txt")
+        .lines()
+        .map(parse)
+        .collect::<Result<_>>()?;
+
+    Ok(count_overlaps(&lines))
+}
+
 #[test]
 fn test_part1() {
-    assert_eq!(4_728, part1());
+    assert_eq!(4_728, part1().unwrap());
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(17_717, part2().unwrap());
 }
