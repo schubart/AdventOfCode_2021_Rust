ddc1c10fbf189916acb4662851b87a60ef762b15
Michael Schubart <michael@schubart.net>
2021-12-09 19:43:18 +0000

    Day 8: Use matches!()

--- day08/src/lib.rs
+++ day08/src/lib.rs
@@ -10,8 +10,7 @@ fn part1() -> usize {
     include_str!("input.txt")
         .lines()
         .flat_map(|l| l.split(' ').skip(11))
-        .map(str::len)
-        .filter(|&l| l == 2 || l == 3 || l == 4 || l == 7)
+        .filter(|&signal| matches!(signal.len(), 2 | 3 | 4 | 7))
         .count()
 }
 
