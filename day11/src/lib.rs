b6137a67080d3cb30915e118d8ae62abd4690e31
Michael Schubart <michael@schubart.net>
2021-12-11 20:14:35 +0000

    day 11 WIP

--- day11/src/lib.rs
+++ day11/src/lib.rs
@@ -2,11 +2,29 @@
 #![warn(clippy::pedantic)]
 
 fn part1() -> usize {
-    let values: Vec<Vec<usize>> = include_str!("input.txt")
+    let mut values: Vec<Vec<usize>> = include_str!("input.txt")
         .lines()
         .map(|line| line.chars().map(|c| (c as usize - '0' as usize)).collect())
         .collect();
-    dbg!(values).len()
+
+    for _ in 0..3 {
+        for x in 0..values.len() {
+            for y in 0..values[0].len() {
+                print!("{}", values[x][y]);
+            }
+            println!();
+        }
+        println!();
+
+        for x in 0..values.len() {
+            for y in 0..values[0].len() {
+                values[x][y] = (values[x][y] + 1) % 10
+            }
+        }
+
+    }
+
+    0
 }
 
 #[test]
