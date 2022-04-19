def87922195eaf33275dc63f92cf98f88ab7aa07
Michael Schubart <michael@schubart.net>
2022-04-19 06:46:06 +0100

    compiles

--- day09/src/lib.rs
+++ day09/src/lib.rs
@@ -1,6 +1,8 @@
 #![cfg(test)]
 //#![warn(clippy::pedantic)]
 
+use std::collections::HashSet;    
+    
 fn get(matrix: &[Vec<u8>], x: isize, y: isize) -> &u8 {
     if y < 0 || y >= (matrix.len() as isize) {
         return &u8::MAX;
@@ -48,4 +50,33 @@ fn part1() -> usize {
 #[test]
 fn test_part1() {
     assert_eq!(524, part1());
+}
+
+type Point = (isize, isize);
+
+#[test]
+fn test_part2() {
+    let heights: Vec<Vec<u8>> = include_str!("input.txt")
+        .lines()
+        .map(|line| line.chars().map(|c| (c as u8) - b'0').collect())
+        .collect();
+
+    let mut points = HashSet::new();
+    
+    for (y, row) in heights.iter().enumerate() {
+        for (x, height) in row.iter().enumerate() {
+            if *height != 9 {
+                let point = (x as isize, y as isize);
+                points.insert(point);
+            }
+        }
+    }
+
+    while let Some(&point) = points.iter().next() {
+        points.remove(&point);
+    }
+}
+
+fn fill(points: &mut HashSet<Point>, point: Point) {
+    points.remove(&point);
 }
